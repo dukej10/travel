65f858ea3af3035b71185c1feef4cf804f32206b
Juan Duque <diegoduqqe@gmail.com>
2024-05-13 19:06:07 -0500

    feat: change name en-point method FlyController

--- src/main/java/com/dukez/best_travel/api/controllers/FlyController.java
+++ src/main/java/com/dukez/best_travel/api/controllers/FlyController.java
@@ -1,25 +1,23 @@
 package com.dukez.best_travel.api.controllers;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dukez.best_travel.api.models.response.FlyResponse;
 import com.dukez.best_travel.infrastructure.abstract_service.IFlyService;
 import com.dukez.best_travel.util.SortType;
-import java.util.Objects;
-import java.util.Set;
-import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @AllArgsConstructor
@@ -30,7 +28,7 @@ public class FlyController {
     private final IFlyService flyService;
 
     @GetMapping(path = "all")
-    public ResponseEntity<Page<FlyResponse>> getAllFlies(@RequestParam Integer page, @RequestParam Integer size,
+    public ResponseEntity<Page<FlyResponse>> getAllFlights(@RequestParam Integer page, @RequestParam Integer size,
             @RequestHeader(required = false) SortType sortType) {
         if (Objects.isNull(sortType))
             sortType = SortType.NONE;

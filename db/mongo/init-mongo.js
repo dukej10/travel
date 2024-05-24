1ff49cc9891d24092cd1c21b7dbb17917348dd43
Juan Duque <diegoduqqe@gmail.com>
2024-05-23 20:17:59 -0500

    feat: update encrypted password in script mongodb

--- db/mongo/init-mongo.js
+++ db/mongo/init-mongo.js
@@ -15,7 +15,7 @@ db.app_users.insert([
     username: "ragnar777",
     dni: "VIKI771012HMCRG093",
     enabled: true,
-    password: "s3cr3t",
+    password: "$2a$10$guY6xP201iZ.16pdhuEdRuUPibqJ3R.hkO14jXOVK6o/N.6Dqq8De",
     role: {
       granted_authorities: ["ROLE_USER"],
     },
@@ -24,7 +24,7 @@ db.app_users.insert([
     username: "heisenberg",
     dni: "BBMB771012HMCRR022",
     enabled: true,
-    password: "p4sw0rd",
+    password: "$2a$10$VTJksn0sZGyLVPx2o6EfJupZfT0LNgle7hXSrkZBzFF8buLqVH4QW",
     role: {
       granted_authorities: ["ROLE_USER"],
     },
@@ -33,7 +33,7 @@ db.app_users.insert([
     username: "misterX",
     dni: "GOTW771012HMRGR087",
     enabled: true,
-    password: "misterX123",
+    password: "$2a$10$xvBa9IZwpCNR/QrMda27jewQmZj6WKaNEqqP/Rlk8OPN53qrugiSW",
     role: {
       granted_authorities: ["ROLE_USER", "ROLE_ADMIN"],
     },
@@ -42,7 +42,7 @@ db.app_users.insert([
     username: "neverMore",
     dni: "WALA771012HCRGR054",
     enabled: true,
-    password: "4dmIn",
+    password: "$2a$10$nWd1Rrq1dBnnR1Kn0MUlZemt6q8LO5KhJ55iZAcfgSYLk8D6CkAzK",
     role: {
       granted_authorities: ["ROLE_ADMIN"],
     },

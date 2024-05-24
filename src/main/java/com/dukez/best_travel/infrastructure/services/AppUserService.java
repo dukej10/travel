568e0769d9a25c285e0ca69cce46ce72f255d4db
Juan Duque <diegoduqqe@gmail.com>
2024-05-23 20:18:41 -0500

    feat: intwgratión security with token

--- src/main/java/com/dukez/best_travel/infrastructure/services/AppUserService.java
+++ src/main/java/com/dukez/best_travel/infrastructure/services/AppUserService.java
@@ -2,19 +2,31 @@ package com.dukez.best_travel.infrastructure.services;
 
 import com.dukez.best_travel.infrastructure.abstract_service.IModifyUserService;
 import com.dukez.best_travel.util.exceptions.UsernameNotFoundException;
+import com.dukez.best_travel.domain.entities.documents.AppUserDocument;
 import com.dukez.best_travel.domain.repositories.mongo.AppUserRepository;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 @Slf4j
-public class AppUserService implements IModifyUserService {
+@Transactional
+public class AppUserService implements IModifyUserService, UserDetailsService {
 
     private final AppUserRepository appUserRepository;
     private final static List<String> AUTHORITIES_PERMIT = List.of("ROLE_USER", "ROLE_ADMIN");
@@ -60,5 +72,30 @@ public class AppUserService implements IModifyUserService {
     }
 
     private static String COLLECTION_NAME = "app_users";
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        var user = this.appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(COLLECTION_NAME));
+        return mapUserToUserDetails(user);
+    }
+
+    private static UserDetails mapUserToUserDetails(AppUserDocument user) {
+        Set<GrantedAuthority> authorities = user.getRole()
+                .getGrantedAuthorities()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+        System.out.println("Authority from db" + authorities);
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                authorities);
+    }
 
 }
